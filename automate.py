3066fe39180c8cd45a16e5e970ba71b3fc400d50
abd-ool <sarkiihima44@gmail.com>
2022-06-14 16:22:41 +0100

    included docx parsing

--- automate.py
+++ automate.py
@@ -2,6 +2,7 @@ import json
 import translate
 from translate import translate_text
 from googletrans import Translator
+from handle_word import Doc
 
 paragraph_count = 0
 data_dict = {}
@@ -11,6 +12,8 @@ def automate(lang_file):
 	global paragraph_count, data_dict
 
 	output = {}
+	output_document = Doc()
+	table = output_document.create_table()
 	
 	with open(lang_file) as json_file:
 		data = json.load(json_file)
@@ -18,14 +21,20 @@ def automate(lang_file):
 		translator = Translator()
 		for key, text in data.items():
 
-			translated_text = translate_text(text, translator, 'french', 'english')
-			print(translated_text)
-			data_dict[str(paragraph_count)] = translated_text
+			translated_text = translate_text(text, translator, 'russian', 'english')
+
+			output_document.add_to_table(text, translated_text)
+
+			data_dict[str(paragraph_count)] = [text, translated_text]
 			paragraph_count += 1
 
+			if key == '188':
+				break
+
+		output_document.save('trans-russian')
 		# print(data_dict)
 
-automate('results.json')
+automate('russian.json')
 
 
 
---
+++ handle_word.py
@@ -0,0 +1,30 @@
+from docx import Document
+
+class Doc:
+
+	def __init__(self):
+		self.document = Document()
+
+	def get_doc(self):
+		return self.document
+
+	def create_table(self):
+		self.table = self.document.add_table(rows=0, cols=2, style='Table Grid')
+		return self.table
+
+	def add_to_table(self, lang_text, trans):
+		data_row = self.table.add_row().cells
+
+		data_row[0].text = lang_text
+		data_row[1].text = trans
+		
+	def save(self, filename):
+
+		# save to file
+		self.document.save(f"{filename}.docx")
+
+
+
+
+
+
